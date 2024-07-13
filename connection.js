b0dca2b4a8054b173c5fbf7bba41c23a30444938
Arfan Anulal <151611444+ArfanAnulal@users.noreply.github.com>
2024-07-13 10:18:08 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 
 // connecting with db
 // mongoose.connect("connection string").then().catch() 
-mongoose.connect("mongodb+srv://Arfan:arfan@cluster0.psoq6vh.mongodb.net/crudbatch2?retryWrites=true&w=majority&appName=Cluster0") //write a database name1 btw / and ?
+mongoose.connect("url to connect") //write a database name1 btw / and ?
 .then(()=>{
     console.log("connected to db")
 })
